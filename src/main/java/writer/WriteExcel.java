1f5c24927eadfbb433f49b611f8b0e7b336542d6
Ivan Tymchenko <tymchenkolviv@gmail.com>
2017-01-12 14:26:00 +0200

    Writer to excel is updated

--- src/main/java/writer/WriteExcel.java
+++ src/main/java/writer/WriteExcel.java
@@ -4,10 +4,13 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormat;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
 
 public class WriteExcel {
 
@@ -18,7 +21,16 @@ public class WriteExcel {
         HSSFCell cell = row.createCell(0);
         cell.setCellValue("1. Cell");
 
+        cell = row.createCell(1);
+        DataFormat format = workbook.createDataFormat();
+        CellStyle dateStyle = workbook.createCellStyle();
+        dateStyle.setDataFormat(format.getFormat("dd.mm.yyyy"));
+        cell.setCellStyle(dateStyle);
+        cell.setCellValue(new Date());
 
+        row.createCell(2).setCellValue("3. Cell");
+
+        sheet.autoSizeColumn(1);
 
         workbook.write(new FileOutputStream("Test_excel.xlsx"));
         workbook.close();
